ef05b544e5739dfbabcea8ec04742f9581ddf599
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2024-04-27 17:24:49 +0200

    cleanup

--- src/clipboard.ts
+++ src/clipboard.ts
@@ -3,46 +3,43 @@ import { clipboard } from "electron";
 import type { ClipItem } from "./types/ClipboardItem";
 
 export function read(): ClipItem | null {
-    const formats = clipboard.availableFormats();
-
-    for (const format of formats) {
-        if (format.startsWith("image/")) {
-            const image = clipboard.readImage();
-            const dataUrl = image.toDataURL();
-            return {
-                type: "image",
-                id: dataUrl,
-                raw: image,
-                dataUrl,
-            };
-        }
-        if (format === "text/plain") {
-            const text = clipboard.readText();
-            return {
-                type: "text",
-                id: text,
-                text,
-            };
-        }
+    const image = clipboard.readImage();
+    if (!image.isEmpty()) {
+        const dataUrl = image.toDataURL();
+        return {
+            type: "image",
+            id: dataUrl,
+            raw: image,
+            dataUrl,
+        };
     }
 
-    console.warn(`Unsupported clipboard formats: [${formats.join(", ")}]`);
+    const text = clipboard.readText();
+    if (text) {
+        return {
+            type: "text",
+            id: text,
+            text,
+        };
+    }
+
+    console.warn(`Unsupported clipboard formats: [${clipboard.availableFormats().join(", ")}]`);
 
     return null;
 }
 
-export function write(data: ClipItem) {
-    switch (data.type) {
+export function write(item: ClipItem) {
+    switch (item.type) {
         case "image":
-            clipboard.writeImage(data.raw);
+            clipboard.writeImage(item.raw);
             break;
         case "text":
-            clipboard.writeText(data.text);
+            clipboard.writeText(item.text);
             break;
     }
 }
 
-export function onChange(callback: (data: ClipItem) => void) {
+export function onChange(callback: (item: ClipItem) => void) {
     clipboardEvent.startListening();
 
     clipboardEvent.on("change", () => {
