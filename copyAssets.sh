5185391089fa7148977ba798650b970b2e6775f9
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2024-06-09 13:04:12 +0200

    Use electron package instead of forge

--- copyAssets.sh
+++ copyAssets.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cp src/index.html dist \
-&& cp node_modules/bootstrap/dist/css/bootstrap.min.css dist \
-&& cp src/renderer/titlebar.css dist \
-&& cp src/renderer/styles.css dist \
-&& cp -r images dist
+cp src/index.html out \
+&& cp node_modules/bootstrap/dist/css/bootstrap.min.css out \
+&& cp src/renderer/titlebar.css out \
+&& cp src/renderer/styles.css out \
+&& cp -r images out
