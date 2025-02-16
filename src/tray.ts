e7d3ce58b97cb29f9ae98ef7ed31f849df0d23ed
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2025-02-16 10:34:52 +0100

    Renamed tray icon exit option

--- src/tray.ts
+++ src/tray.ts
@@ -11,7 +11,7 @@ export async function createTray(iconPath: string) {
 
     const contextMenu = Menu.buildFromTemplate([
         {
-            label: "Exit",
+            label: `Exit ${NAME}`,
             type: "normal",
             click: () => runCommand({ id: "exit" }),
         },
