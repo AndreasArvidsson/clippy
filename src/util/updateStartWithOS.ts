f4e5ca1e5bd9467cca4cc27d0a2da52a5c65ebcb
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2025-03-07 02:19:15 +0100

    Catch failing to set startWithOS

--- src/util/updateStartWithOS.ts
+++ src/util/updateStartWithOS.ts
@@ -1,10 +1,15 @@
 import { app } from "electron";
+import { showErrorNotification } from "./notifications";
 import { isProduction } from "./runMode";
 
 export function updateStartWithOS(startWithOS: boolean): void {
     if (isProduction()) {
-        app.setLoginItemSettings({
-            openAtLogin: startWithOS,
-        });
+        try {
+            app.setLoginItemSettings({
+                openAtLogin: startWithOS,
+            });
+        } catch (error) {
+            showErrorNotification("Failed to update start with OS", error);
+        }
     }
 }
