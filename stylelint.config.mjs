c37e78137eff50ca697627bb097382052ec8e618
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2026-03-29 14:57:21 +0200

    Added stylelint

---
+++ stylelint.config.mjs
@@ -0,0 +1,7 @@
+/** @type {import('stylelint').Config} */
+const config = {
+    extends: ["stylelint-config-standard"],
+};
+
+// oxlint-disable-next-line import/no-default-export
+export default config;
